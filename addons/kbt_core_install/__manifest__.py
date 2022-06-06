1391b2d433aae3304532fe776eba69e75325f40a
PdcShoun <pdc.pachara@gmail.com>
2022-06-06 16:19:10 +0700

    [IMP]PaymentApi: create file

--- addons/kbt_core_install/__manifest__.py
+++ addons/kbt_core_install/__manifest__.py
@@ -5,8 +5,10 @@
     'website': 'https://roots.tech',
     'version': '15.0.0.0.1',
     'depends': [
+        'kbt_business_type',
         'kbt_core_update',
         'kbt_partner_api',
+        'kbt_payment_api',
         'kbt_res_company',
         'kbt_sale_order_api'
     ]
---
+++ addons/kbt_payment_api/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'KBT Payment Api',
+    'summary': '''
+        KBT Payment Api
+    ''',
+    'author': 'Roots',
+    'category': 'Company',
+    'website': 'https://roots.tech',
+    'version': '15.0.0.0.1',
+    'license': 'LGPL-3',
+    'contributors': [
+        'Pachara P.',
+    ],
+    'depends': [
+        'payment',
+        'kbt_core_update',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ]
+}
