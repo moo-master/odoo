903c0f0e0deda12e9ed7afec9760f44a28e0139c
Thitiwat Kulthammanit(Bank) <this.velocityz@gmail.com>
2022-06-27 17:46:00 +0700

    Make res_partner = x_interface_id and add x_is_interface field

--- addons/kbt_partner_api/models/res_partner.py
+++ addons/kbt_partner_api/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartnerInherit(models.Model):
@@ -8,3 +8,19 @@ class ResPartnerInherit(models.Model):
         string='Partner Interface',
         readonly=True,
     )
+
+    # pylint: disable=biszx-boolean-field-name
+    x_is_interface = fields.Boolean(
+        string='Interface',
+    )
+
+    @api.depends('x_interface_id')
+    def _compute_x_interface_id(self):
+        for rec in self:
+            if rec.x_interface_id:
+                rec.ref = rec.x_interface_id
+
+    @api.depends('x_is_interface')
+    def _compute_x_is_interface(self):
+        for rec in self:
+            rec.ref.readonly = rec.x_is_interface
