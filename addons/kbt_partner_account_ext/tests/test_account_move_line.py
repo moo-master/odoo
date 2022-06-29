4d6ddbf5ecdd9d76b52a8b6a3908db225cfbd5c6
Thitiwat Kulthammanit(Bank) <this.velocityz@gmail.com>
2022-06-29 12:05:35 +0700

    Fix test_account_move_line

--- addons/kbt_partner_account_ext/tests/test_account_move_line.py
+++ addons/kbt_partner_account_ext/tests/test_account_move_line.py
@@ -18,16 +18,74 @@ def currency(env):
     })
 
 
+@pytest.fixture
+def account_type(env):
+    return env['account.account.type'].create({
+        'name': 'Test Type',
+        'type': 'other',
+        'internal_group': 'income',
+    })
+
+
+@pytest.fixture
+def account_receivable(env, account_type):
+    return env['account.account'].create({
+        'code': '1000',
+        'name': 'Test Receivable Account',
+        'user_type_id': account_type.id
+    })
+
+
+@pytest.fixture
+def account_payable(env, account_type):
+    return env['account.account'].create({
+        'code': '2000',
+        'name': 'Test Payable Account',
+        'user_type_id': account_type.id
+    })
+
+
+@pytest.fixture
+def payment_term(env):
+    return env['account.payment.term'].create({
+        'name': 'the 15th of the month, min 31 days from now',
+        'line_ids': [
+                (0, 0, {
+                    'value': 'balance',
+                    'days': 31,
+                    'day_of_the_month': 15,
+                    'option': 'day_after_invoice_date',
+                }),
+        ],
+    })
+
+
+@pytest.fixture
+def partner(env, payment_term, account_receivable, account_payable):
+    return env['res.partner'].create({
+        'name': 'Your Supplier',
+        'email': 'supplier@other.company.com',
+        'supplier_rank': 10,
+        'company_id': 1,
+        'property_payment_term_id': payment_term.id,
+        'property_account_receivable_id': account_receivable.id,
+        'property_account_payable_id': account_payable.id,
+        'x_offset': True,
+    })
+
+
 @pytest.mark.parametrize('test_input', [
     ({'type': 'out_invoice', 'type_debit': 'out_debit'}),
     ({'type': 'in_invoice', 'type_debit': 'in_debit'})
 ])
-def test_x_offset(model, env, test_input, currency):
+def test_create_model_with_x_offset(model, env, test_input, currency, partner):
     acc_move = env['account.move'].with_context({
         'default_move_type': test_input['type']
-    }).create({})
+    }).create({'partner_id': partner.id})
     vals = {'move_id': acc_move.id,
             'currency_id': currency.id,
-            'x_offset': True}
-    res = model.create(vals)
-    assert res.x_offset
+            'product_id': env.ref('product.product_product_2').id,
+            'price_unit': 642.0,
+            'quantity': 1}
+    res = model.new(vals)
+    assert res.x_offset == res.move_id.partner_id.x_offset
