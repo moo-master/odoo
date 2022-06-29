e9b8a6dc5eb7ca927403126b45e3cc8167935823
PdcShoun <pdc.pachara@gmail.com>
2022-06-29 10:23:26 +0700

    [ADD]GoodReceiptApi: create new addon

---
+++ addons/kbt_good_receipt_api/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'KBT Good Receipt Api',
+    'summary': '''
+        KBT Good Receipt Api
+    ''',
+    'author': 'Roots',
+    'category': 'Company',
+    'website': 'https://roots.tech',
+    'version': '15.0.0.0.1',
+    'license': 'LGPL-3',
+    'contributors': [
+        'Pachara P.',
+    ],
+    'depends': [
+        'kbt_business_type',
+        'kbt_stock_picking_ext',
+    ],
+}
