90f94e24bcefb5f054753e48758737b83d07f6c9
Thitiwat Kulthammanit(Bank) <this.velocityz@gmail.com>
2022-07-01 16:42:46 +0700

    Add unit test on test_create_res_partner

--- addons/kbt_partner_api/tests/test_res_partner.py
+++ addons/kbt_partner_api/tests/test_res_partner.py
@@ -12,6 +12,7 @@ def test_create_partner(model):
         'name': 'Your Supplier',
         'email': 'supplier@other.company.com',
         'company_id': 1,
-        'x_interface_id': 'Test-id-001'
+        'x_interface_id': 'Test-id-001',
     })
     assert res_partner.x_interface_id
+    assert res_partner.ref == res_partner.x_interface_id
