a4e0fcb409c41409318c7ba3bb4f373866717a7e
PdcShoun <pdc.pachara@gmail.com>
2022-09-14 11:47:17 +0700

    [IMP]AccountExt: JS footer change params

--- addons/kbt_account_ext/static/src/js/force_new_page.js
+++ addons/kbt_account_ext/static/src/js/force_new_page.js
@@ -1,33 +1,33 @@
-// data_page_size full page without footer
-// data_page_size_detail full page with footer
-// page_body_set data page check by last line bottom AND top footer
+// data-page-size full page without footer
+// data-page-size_detail full page with footer
+// page-body-set data page check by last line bottom AND top footer
 
 const elem = document.getElementsByClassName("new-page");
 const rect = elem[0].getBoundingClientRect();
 
-const data_page_size = elem[0].getAttribute("data_page_size");
-const data_page_size_detail = elem[0].getAttribute("data_page_size_detail");
+const data_page_size = elem[0].getAttribute("data-page-size");
+const data_page_size_detail = elem[0].getAttribute("data-page-size-detail");
 
-const page_body = document.getElementsByClassName("page_body_set");
-const page_size_body = page_body[0].getAttribute("data_body_set");
+const page_body = document.getElementsByClassName("page-body-set");
+const page_size_body = page_body[0].getAttribute("data-body-set");
 
 const page_last = document.getElementsByClassName("last-footer");
 
 // Find data page
-// const page_data = document.getElementById("data-page")
-// page_data.innerHTML = elem.getBoundingClientRect().top
+// const page_data = document.getElementById("data-page") <= <div class="data-page"/> under LineOrder
+// page_data.innerHTML = elem.getBoundingClientRect().top <= print data PageHeight
 
 
 if ((rect.top % data_page_size) >= data_page_size_detail) {
-    var newdiv = document.getElementsByClassName("new_page");
+    var newdiv = document.getElementsByClassName("new-page");
     newdiv[0].setAttribute('style', 'page-break-after:always');
-    size_relative = (Math.ceil(rect.top / data_page_size) + 1) * page_size_body
+    size_relative = (Math.ceil(rect.top / data_page_size) + 1) * 205
 }
 else {
-    size_relative = (Math.ceil(rect.top / data_page_size)) * page_size_body
+    size_relative = (Math.ceil(rect.top / data_page_size)) * 155
 }
 page_body[0].style.position = 'relative';
 page_body[0].style.height = size_relative + 'mm';
-page_last[0].style.position = 'absolute';
+page_last[0].style.position = 'static';
 page_last[0].style.bottom = '0mm';
 page_last[0].style.width = '100%';
