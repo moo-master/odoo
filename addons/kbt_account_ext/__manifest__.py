ce6539d3386d2c824edf1cd09ebe6b671b6953fb
JaOwARaiWa <kiattikun.tho@ku.th>
2022-09-15 11:33:11 +0700

    [ADD]AccountExt: add new model 'account_payment'

--- addons/kbt_account_ext/__manifest__.py
+++ addons/kbt_account_ext/__manifest__.py
@@ -8,11 +8,17 @@
     'website': 'https://roots.tech',
     'version': '15.0.0.0.1',
     'license': 'LGPL-3',
+    'external_dependencies': {
+        'python': [
+            'bahttext',
+        ],
+    },
     'contributors': [
         'Pachara P.',
     ],
     'depends': [
         'kbt_business_type',
+        'kbt_account_wht_ext',
     ],
     'data': [
         'report/receipt_tax_invoice_report.xml',
