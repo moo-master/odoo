e7e275976a3eb92509a0d7a8e92b6fa5a235de6a
PdcShoun <pdc.pachara@gmail.com>
2022-10-03 18:31:51 +0700

    [IMP]AccountExt: create new field

--- addons/kbt_account_ext/models/account_move.py
+++ addons/kbt_account_ext/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 from bahttext import bahttext
 
@@ -13,6 +13,40 @@ class AccountMove(models.Model):
         ],
         ondelete={'in_refund': 'set default', 'out_refund': 'set default'}
     )
+    x_old_tax_inv_amount = fields.Float(
+        string='Old Tax Invoice Amount',
+        compute='_compute_old_invoice_amount',
+        store=True,
+    )
+    x_wht_amount = fields.Float(
+        string='WHT Amount',
+        compute='_compute_old_invoice_amount',
+        store=True,
+    )
+    x_real_amount = fields.Float(
+        string='Real Amount',
+    )
+    x_diff_amount = fields.Float(
+        string='Diff Amount',
+    )
 
     def get_amount_total_text(self, amount):
         return bahttext(amount)
+
+    @api.depends('x_real_amount')
+    def _compute_old_invoice_amount(self):
+        for move in self:
+            invoice = self
+            move.write({
+                'x_old_invoice_amount': invoice.amount_untaxed,
+                'x_diff_amount': move.x_old_tax_inv_amount - move.x_real_amount,
+                'x_wht_amount': invoice.amount_wht,
+            })
+
+        return
+
+    @api.onchange('x_real_amount')
+    def _onchange_x_real_amount(self):
+        self.write({
+            'x_diff_amount': self.x_old_tax_inv_amount - self.x_real_amount
+        })
