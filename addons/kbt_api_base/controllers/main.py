410e2726990bfcacb6cdd9a3dc7e251a3c22318b
PdcShoun <pdc.pachara@gmail.com>
2022-11-02 17:39:45 +0700

    [IMP]SalePurchaseApi: edit check WHT before create

--- addons/kbt_api_base/controllers/main.py
+++ addons/kbt_api_base/controllers/main.py
@@ -17,3 +17,25 @@ class KBTApiBase(APIBase):
             })
         response_api.update(kwargs)
         return response_api
+
+    def _check_wht_sequence(self, order_lines, check_val):
+        section_5_list = []
+        section_6_list = []
+        for line in order_lines:
+            if (self.section_check((seq := int(line.get(check_val)))) == 5
+                    and seq not in section_5_list):
+                section_5_list.append(seq)
+            if (self.section_check(seq) == 6
+                    and seq not in section_6_list):
+                section_6_list.append(seq)
+        if len(section_5_list) > 1 or len(section_6_list) > 1:
+            raise ValueError(
+                "You can not select different WHT under the same category."
+                " Right now your section 5 or section 6"
+                " are under the same category.")
+
+    def section_check(self, sequence):
+        if(int(sequence / 500) == 1 and (sequence % 500) < 100):
+            return 5
+        if(int(sequence / 600) == 1 and (sequence % 600) < 100):
+            return 6
