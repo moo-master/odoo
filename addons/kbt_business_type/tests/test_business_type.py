f502aec6bf0aa906b4aea2ef2e9f0da3bddedc40
PdcShoun <pdc.pachara@gmail.com>
2022-11-24 15:08:14 +0700

    [IMP]BusinessType: add func _onchange_is_unearn_revenue

---
+++ addons/kbt_business_type/tests/test_business_type.py
@@ -0,0 +1,17 @@
+from pytest_tr_odoo.fixtures import env
+import pytest
+
+
+@pytest.fixture
+def model(env):
+    return env['business.type']
+
+
+def test__onchange_is_unearn_revenue(env, model):
+    acc = env['account.account'].search([])
+    business_type = model.new({
+        'x_revenue_account_id': 1,
+        'is_unearn_revenue': False
+    })
+    business_type._onchange_is_unearn_revenue()
+    assert not business_type.x_revenue_account_id
