9cafeb299e2cc98f4922c62e65f67f1b8f005d40
PdcShoun <pdc.pachara@gmail.com>
2023-01-18 18:31:19 +0700

    [IMP]AccountExt: add new model account.account.group

--- addons/kbt_account_ext/models/__init__.py
+++ addons/kbt_account_ext/models/__init__.py
@@ -2,3 +2,5 @@ from . import sale_order
 from . import account_payment_term
 from . import account_payment
 from . import account_move
+from . import account_account
+from . import account_account_group
