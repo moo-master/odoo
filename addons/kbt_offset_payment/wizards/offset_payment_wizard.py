c9fd52d4ec935b8da8385f5e1f0fd65e96387c9a
PdcShoun <pdc.pachara@gmail.com>
2023-02-15 15:42:52 +0700

    [IMP]OffsetPayment: edit button_confirm use journal from field

--- addons/kbt_offset_payment/wizards/offset_payment_wizard.py
+++ addons/kbt_offset_payment/wizards/offset_payment_wizard.py
@@ -94,7 +94,7 @@ class OffsetPaymentWizard(models.TransientModel):
                     _("""Waning! This process cannot continues because the amount to be processed is greater than the amount of this document"""))
             payment_type = 'inbound' if move.move_type == 'out_invoice' else 'outbound'
             partner_type = 'customer' if move.move_type == 'out_invoice' else 'supplier'
-            journal_id = self._get_bank_journal()
+            journal_id = self.journal_id
             payment_method_line_id = self._get_manual_payment_method(
                 journal_id, payment_type)
             move_payment_val = {
