a68fa17441e3dc307b6112916f123f969e31b919
nattikan09 <nattikan.o@gmail.com>
2023-04-21 09:35:09 +0700

    fix js

--- additional-addons/password_security/__manifest__.py
+++ additional-addons/password_security/__manifest__.py
@@ -30,7 +30,7 @@
     "assets": {
         "web.assets_common": [
             "/password_security/static/src/js/password_gauge.js",
-            "/password_security/static/่js/zxcvbn/zxcvbn.min.js",
+            "/password_security/static/src/่js/zxcvbn/zxcvbn.min.js",
         ],
     },
     "demo": [
