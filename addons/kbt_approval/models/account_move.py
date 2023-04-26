23168d7c51d32428e9844160ca0365c14edde17f
nattikan09 <nattikan.o@gmail.com>
2023-04-26 14:46:23 +0700

    fix exception

--- addons/kbt_approval/models/account_move.py
+++ addons/kbt_approval/models/account_move.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api, _
 # from odoo.exceptions import ValidationError, _
-from odoo import exceptions,except_orm, _
+from odoo import exceptions, _
 
 
 class AccountMove(models.Model):
