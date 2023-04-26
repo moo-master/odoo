3f3ba33ffa5c50ff668af64da542fd5f9aa6849c
nattikan09 <nattikan.o@gmail.com>
2023-04-26 14:19:48 +0700

    add exceptions.

--- addons/kbt_approval/models/account_move.py
+++ addons/kbt_approval/models/account_move.py
@@ -210,7 +210,7 @@ class AccountMove(models.Model):
             if not employee.parent_id.level_id:
                 # Back old raise
                 # raise ValidationError(_('Your manager do not have level.'))
-                raise except_orm(_('My Title'), _('Your manager do not have level.'))
+                raise exceptions.except_orm(_('My Title'), _('Your manager do not have level.'))
             if (employee.parent_id.level_id.level - employee.level_id.level
                     > 1) and (not self.is_skip_level) and (not employee.parent_id):
                 self.is_skip_level = True
